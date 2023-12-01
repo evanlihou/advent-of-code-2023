3e730a95f8af4191e6daa6b096745d516286d9b2
evanlihou <evan@evanlihou.com>
2023-12-01 16:48:22 -0500

    Day 1

---
+++ AdventOfCode2023/BaseDay.cs
@@ -0,0 +1,47 @@
+﻿using System.CommandLine;
+
+namespace AdventOfCode2023;
+
+public abstract class BaseDay
+{
+    protected virtual string DayName => "NO NAME";
+    protected async Task<int> RunMain(string[] args)
+    {
+        var fileOpt = new Option<FileInfo?>(name: "--file");
+        fileOpt.AddAlias("-f");
+
+        var rootCommand = new RootCommand($"Day {DayName}");
+        rootCommand.AddGlobalOption(fileOpt);
+
+        var part1Command = new Command("part1");
+        part1Command.SetHandler(file => ReadFile(file, Part1), fileOpt);
+        rootCommand.AddCommand(part1Command);
+        
+        var part2Command = new Command("part2");
+        part2Command.SetHandler(file => ReadFile(file, Part2), fileOpt);
+        rootCommand.AddCommand(part2Command);
+        
+        var bothPartsCommand = new Command("bothparts");
+        bothPartsCommand.SetHandler(file =>
+        {
+            ReadFile(file, Part1);
+            ReadFile(file, Part2);
+        }, fileOpt);
+        rootCommand.AddCommand(bothPartsCommand);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    private static void ReadFile(FileInfo? file, Func<IEnumerable<string>, Task> next)
+    {
+        if (file is null || !file.Exists)
+        {
+            throw new ApplicationException("File does not exist");
+        }
+
+        next(File.ReadLines(file.FullName));
+    }
+
+    protected abstract Task Part1(IEnumerable<string> lines);
+    protected abstract Task Part2(IEnumerable<string> lines);
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,92 @@
+﻿// using AdventOfCode2023;
+
+using AdventOfCode2023;
+
+namespace Day1;
+
+internal class Day1 : BaseDay
+{
+    private static readonly Dictionary<string, int> DigitMapping = new()
+    {
+        { "0", 0 },
+        { "1", 1 },
+        { "2", 2 },
+        { "3", 3 },
+        { "4", 4 },
+        { "5", 5 },
+        { "6", 6 },
+        { "7", 7 },
+        { "8", 8 },
+        { "9", 9 }
+    };
+    
+    private static readonly Dictionary<string, int> WordDigitMapping = new()
+    {
+        { "zero", 0 },
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 }
+    };
+    
+    protected override string DayName => "1";
+
+    protected override Task Part1(IEnumerable<string> lines)
+    {
+        Console.WriteLine($"Part 1 answer: {Common(lines, DigitMapping)}");
+        return Task.CompletedTask;
+    }
+
+    protected override Task Part2(IEnumerable<string> lines)
+    {
+        Console.WriteLine(
+            $"Part 2 answer: {Common(lines, DigitMapping.Concat(WordDigitMapping).ToDictionary())}");
+        return Task.CompletedTask;
+    }
+
+    private static int Common(IEnumerable<string> lines, Dictionary<string, int> entries)
+    {
+        var sum = 0;
+        foreach (var l in lines)
+        {
+            var line = l;
+            int? firstDigit = null;
+            do
+            {
+                foreach (var entry in entries.Where(entry => line.StartsWith(entry.Key)))
+                {
+                    firstDigit = entry.Value;
+                }
+
+                if (firstDigit is null) line = line[1..];
+            } while (firstDigit is null && line.Length > 0);
+            
+            int? lastDigit = null;
+            do
+            {
+                foreach (var entry in entries.Where(entry => line.EndsWith(entry.Key)))
+                {
+                    lastDigit = entry.Value;
+                }
+
+                if (lastDigit is null) line = line[..^1];
+            } while (lastDigit is null && line.Length > 0);
+
+            if (firstDigit is null || lastDigit is null) continue;
+            var num = firstDigit * 10 + lastDigit;
+            sum += num.Value;
+        }
+        
+        return sum;
+    }
+
+    internal static async Task<int> Main(string[] args)
+    {
+        return await new Day1().RunMain(args);
+    }
+}
